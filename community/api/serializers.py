136ac7405bf45a3fc39b70e15edfd7b45d39674d
Jenil Savla <savlajenil480@gmail.com>
2023-04-04 06:01:42 +0000

    Production level changes

--- community/api/serializers.py
+++ community/api/serializers.py
@@ -3,7 +3,7 @@ from .models import User, Family, OccupationAddress, Event, Company, Job, Matrim
 
 class LoginSerializer(serializers.ModelSerializer):
     password=serializers.CharField(max_length=32,min_length=8,write_only = True)
-    
+
     class Meta:
         model = User
         fields = ['username','password']
@@ -42,3 +42,40 @@ class MatrimonySerializer(serializers.ModelSerializer):
 	class Meta:
 		model =  Matrimony
 		fields = '__all__'
+
+class MemberListSerializer(serializers.ModelSerializer):
+	home_address = serializers.SerializerMethodField(read_only=True)
+	gotrej = serializers.SerializerMethodField(read_only=True)
+	class Meta:
+		model = User
+		fields = ["is_superuser",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "phone",
+            "name",
+            "relation",
+            "dob",
+            "education",
+            "profession_status",
+            "profession_name",
+            "native_village",
+            "gender",
+            "blood_group",
+            "maritial_status",
+	    	"related_family",
+	    	"related_company",
+	    	"related_matrimony",
+	    	"home_address",
+		    "gotrej"
+			]
+
+	def get_home_address(self,obj):
+		family = Family.objects.get(id=obj.related_family.id)
+		serializer = FamilySerializer(family)
+		return serializer.data['home_address']
+
+	def get_gotrej(self,obj):
+		family = Family.objects.get(id=obj.related_family.id)
+		serializer = FamilySerializer(family)
+		return serializer.data['gotrej']
